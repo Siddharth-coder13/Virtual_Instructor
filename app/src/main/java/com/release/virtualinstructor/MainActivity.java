5a55698aee75c13d527a8fd98aa282e1d930d532
Siddharth-coder13 <sid021099kumar@gmail.com>
2020-10-12 23:19:38 +0530

    Added bottom navigation view and different fragments (Yoga and meditation)

--- app/src/main/java/com/release/virtualinstructor/MainActivity.java
+++ app/src/main/java/com/release/virtualinstructor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.release.virtualinstructor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //to start with yoga fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new yoga_fragment()).commit();
+
+        //bottom navigation view
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.yoga_nav :
+                        selectedFragment = new yoga_fragment();
+                        break;
+                    case R.id.meditation_nav :
+                        selectedFragment = new meditation_fragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                return true;
+            }
+        });
+
     }
 }
