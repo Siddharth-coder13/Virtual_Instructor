d50650882c9f7db2798a4db3d5221c5307a5f070
Siddharth-coder13 <sid021099kumar@gmail.com>
2020-10-30 23:46:23 +0530

    Added channel 1 activity

---
+++ app/src/main/java/com/release/virtualinstructor/channel_list.java
@@ -0,0 +1,14 @@
+package com.release.virtualinstructor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class channel_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_list);
+    }
+}
