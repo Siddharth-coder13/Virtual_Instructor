31485f4713817254982f4f44f7d9ded4283c2793
Keerthana1102 <keerthana.nuti11@gmail.com>
2020-11-10 00:31:31 +0530

    Added and linked yoga channel lists and environment intent.

---
+++ app/src/main/java/com/release/virtualinstructor/channel_yoga_list/yoga_channel1_list.java
@@ -0,0 +1,83 @@
+package com.release.virtualinstructor.channel_yoga_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.release.virtualinstructor.R;
+import com.release.virtualinstructor.environment_list;
+
+public class yoga_channel1_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoga_channel1_list);
+
+        // on clicking back button
+        ImageView back = findViewById(R.id.yoga_channel1_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //This java file is used to navigate to environment_list activity
+
+        CardView yoga1 = findViewById(R.id.channel1_yoga1);
+        TextView text1 = findViewById(R.id.yoga1_text);
+
+        CardView yoga2 = findViewById(R.id.channel1_yoga2);
+        TextView text2 = findViewById(R.id.yoga2_text);
+
+        CardView yoga3 = findViewById(R.id.channel1_yoga3);
+        TextView text3 = findViewById(R.id.yoga3_text);
+
+        CardView yoga4 = findViewById(R.id.channel1_yoga4);
+        TextView text4 = findViewById(R.id.yoga4_text);
+
+        CardView yoga5 = findViewById(R.id.channel1_yoga5);
+        TextView text5 = findViewById(R.id.yoga5_text);
+
+        CardView yoga6 = findViewById(R.id.channel1_yoga6);
+        TextView text6 = findViewById(R.id.yoga6_text);
+
+        CardView yoga7 = findViewById(R.id.channel1_yoga7);
+        TextView text7 = findViewById(R.id.yoga7_text);
+
+        CardView yoga8 = findViewById(R.id.channel1_yoga8);
+        TextView text8 = findViewById(R.id.yoga8_text);
+
+        CardView yoga9 = findViewById(R.id.channel1_yoga9);
+        TextView text9 = findViewById(R.id.yoga9_text);
+
+        //To open environment_list when med card view is tapped.
+        openEnvironmentList(yoga1,text1);
+        openEnvironmentList(yoga2,text2);
+        openEnvironmentList(yoga3,text3);
+        openEnvironmentList(yoga4,text4);
+        openEnvironmentList(yoga5,text5);
+        openEnvironmentList(yoga6,text6);
+        openEnvironmentList(yoga7,text7);
+        openEnvironmentList(yoga8,text8);
+        openEnvironmentList(yoga9,text9);
+
+    }
+    private void openEnvironmentList(CardView yoga, final TextView text){
+        yoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent i = new Intent(yoga_channel1_list.this, environment_list.class);
+                i.putExtra("heading",text.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
--- app/src/main/java/com/release/virtualinstructor/yoga_fragment.java
+++ app/src/main/java/com/release/virtualinstructor/yoga_fragment.java
@@ -1,26 +1,28 @@
 package com.release.virtualinstructor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
+import com.release.virtualinstructor.channel_yoga_list.yoga_channel1_list;
+import com.release.virtualinstructor.channel_yoga_list.yoga_channel2_list;
+
+// import java.util.ArrayList;
 
 public class yoga_fragment extends Fragment {
-
-    SearchView searchView;
-    ListView yogaList;
-
-    ArrayList<String> list;
-    ArrayAdapter<String> adapter;
+//
+//    SearchView searchView;
+//    ListView yogaList;
+//
+//    ArrayList<String> list;
+//    ArrayAdapter<String> adapter;
 
     @Nullable
     @Override
@@ -28,28 +30,48 @@ public class yoga_fragment extends Fragment {
         View v =  inflater.inflate(R.layout.yoga_fragment,container,false);
 
 
-        searchView = (SearchView) v.findViewById(R.id.yogaSearch);
-        yogaList = (ListView) v.findViewById(R.id.yogaList);
+//        searchView = (SearchView) v.findViewById(R.id.yogaSearch);
+//        yogaList = (ListView) v.findViewById(R.id.yogaList);
+//
+//        list = new ArrayList<String>();
+//        list.add("Basic Poses for Beginners");
+//        list.add("Instructor");
+//
+//        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,list);
+//        yogaList.setAdapter(adapter);
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                adapter.getFilter().filter(newText);
+//                return false;
+//            }
+//        });
 
-        list = new ArrayList<String>();
-        list.add("Basic Poses for Beginners");
-        list.add("Instructor");
+        RelativeLayout yogaChannel1 = v.findViewById(R.id.yoga_channel1_layout);
+        RelativeLayout yogaChannel2 = v.findViewById(R.id.yoga_channel2_layout);
 
-        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,list);
-        yogaList.setAdapter(adapter);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        yogaChannel1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), yoga_channel1_list.class);
+                startActivity(i);
             }
         });
+
+        yogaChannel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), yoga_channel2_list.class);
+                startActivity(i);
+            }
+        });
+
         return v;
     }
 }
